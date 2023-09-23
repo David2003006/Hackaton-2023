1715f7f0fd87e4cb27b490e20697b673351fb709
David2003006 <davidalejandro2003@yahoo.com>
2023-09-23 14:05:06 -0700

    lo uso para simular a un usuario

---
+++ products.py
@@ -0,0 +1,5 @@
+products= [
+    {"id":1, "name":"lapiz", "price":20, "quatity":4},
+    {"id":2, "name":"pluma", "price":15, "quatity":5},
+    {"id":3, "name":"cuadeno", "price":25, "quatity":10}
+]
